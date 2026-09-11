402de99e5e0cc8856b9864fd600af336ec8bd690
mimsha7 <rahman15-6119@diu.edu.bd>
2026-09-12 01:54:28 +0600

    updated

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -7,14 +7,14 @@ export default function Nav({ coin }: { coin: number }) {
     <div className='bg-white'>
         <nav className='flex justify-between items-center p-2 text-md text-black max-w-6xl mx-auto'>
             <img src={logo} alt="Logo" />
-            <ul className='md:flex space-x-4 hidden'>
+            <ul className='md:flex ml-auto text-[16px] text-[#6b6a6a] font-medium space-x-4 hidden [&>li]:cursor-pointer'>
                 <li>Home</li>
                 <li>Fixture</li>
                 <li>Teams</li>
                 <li>Schedules</li>
             </ul>
-            <div className="md:flex items-center hidden">
-                <button className='border border-gray-200 px-4 py-3 hover:bg-gray-100 rounded-xl shadow-sm flex items-center gap-1 p-2'>
+            <div className="md:flex items-center hidden ml-6">
+                <button className='border border-gray-100 px-4 py-3 rounded-xl shadow-xs flex items-center gap-1 p-2'>
                     {coin} Coin <AiFillDollarCircle className='text-amber-500 size-6' />
                 </button>
             </div>
